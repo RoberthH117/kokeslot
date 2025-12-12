01a6d8508d56d6ff6e10b9187ac9b0e9246e0bc2
RoberthH117 <roberthhuchim1@gmail.com>
2025-12-11 22:56:16 -0600

    fixed variables entorno

---
+++ kokeslot/src/assets/env.js
@@ -0,0 +1,9 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  window.__env.API_KEY = "";
+  window.__env.AFFILIATE_UID = "";
+  window.__env.API_STATS = "";
+  window.__env.API_VALIDATE = "";
+
+})(this);
